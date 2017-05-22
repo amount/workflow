ba890dc28e837225615e3c3bd3f45621359e1a41
Chris Moylan <chris.moylan@avant.com>
2017-05-22 14:42:26 -0500

    update tests for activerecord/model 5

--- test/advanced_hooks_and_validation_test.rb
+++ test/advanced_hooks_and_validation_test.rb
@@ -4,6 +4,7 @@ $VERBOSE = false
 require 'active_record'
 require 'sqlite3'
 require 'workflow'
+require 'pry'
 
 ActiveRecord::Migration.verbose = false
 
@@ -47,7 +48,10 @@ class Article < ActiveRecord::Base
         fields_to_validate = meta[:validates_presence_of]
         if fields_to_validate
           validations = Proc.new {
-            errors.add_on_blank(fields_to_validate) if fields_to_validate
+            fields_to_validate.each do |field|
+              value = read_attribute_for_validation(field)
+              errors.add(field, :empty) if value.nil? || value.empty?
+            end
           }
         end
 
